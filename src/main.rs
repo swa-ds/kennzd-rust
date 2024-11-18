3c66c4ed1dfe91ef04ea9d9f7f699e1831b7369d
Stefan Waldmann <Stefan.Waldmann@doubleSlash.de>
2024-11-18 17:08:56 +0100

    adjust styles, externalize CSS styles

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod repo;
 mod ui;
 mod api;
 
+use rocket::fs::FileServer;
 use rocket_dyn_templates::Template;
 
 use repo::KennzdRepo;
@@ -16,6 +17,7 @@ fn rocket() -> _ {
         .manage(KennzdRepo::create())
         .attach(Template::fairing())
         .mount("/", routes![ui::index, ui::ui])
+        .mount("/static", FileServer::from("static"))
         .mount("/api", routes![api::kennz_by_kuerzel, api::kennz_all])
         .register("/api", catchers![api::not_found])
 }
